779589a69c73b297a26d7f2a7a9ecb62af023ee7
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-10-03 09:22:23 +0200

    example2_JS-HTML

---
+++ 2.example/script.js
@@ -0,0 +1,16 @@
+function generate(){
+
+let number1 = document.getElementById("field1").value;
+
+let number2 = document.getElementById("field2").value;
+
+let amountOfRandomNumbers = parseInt(number2) - parseInt(number1);
+
+
+
+console.log(number1 + " : " + number2);
+
+let randomNumber = Math.floor(Math.random() *amountOfRandomNumbers) + parseInt(number1);
+
+document.getElementById("randomNumber").innerHTML = randomNumber;
+}
