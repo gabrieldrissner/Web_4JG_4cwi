907367a946b4a232117e73a3758e4b1725222528
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-11-25 11:05:13 +0100

    need help to bring the data into my tabledesign

--- FootballAPI/index-script.js
+++ FootballAPI/index-script.js
@@ -1,19 +1,33 @@
-const loadData = () => {
-  fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
-    result.json().then((data) => {
-      console.log(data);
-      fillTable(data);
-    });
+function loadTable() {
+  fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().then((data) => {
+      insertTable(data);
+  }))
+
+}
+
+function insertTable(data) {
+  console.log("data", data);
+  const element = document.getElementById("tabelle");
+
+  let output = '';
+  data.forEach((team, index) => {
+      output += ' <div class="grid grid-cols-[20px_40px_auto_40px] p-2 rounded-xl border border-black border-solid mb-2 hover:bg-slate-400">';
+      output += "<div>" + (index + 1) + "</div>";
+      output += ' <div class="w-5"> <img src="' + team.teamIconUrl + '"/></div>';
+      output += " <div>" + team.teamName + "</div>";
+      output += ' <div>' + team.points + '</div>';
+      output += '</div>';
   });
-};
 
-const fillTable = (data) => {
-  let html = "";
+  console.log("output", output);
+  element.innerHTML = output;
 
-  data.forEach((element) => {
-    html += "<div>" + element.teamName + "</div>";
-  });
-  document.getElementById("output").innerHTML = data;
-};
 
-loadData();
+}
+
+
+loadTable();
+
+
+
+
