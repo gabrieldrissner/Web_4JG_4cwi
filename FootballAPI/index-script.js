1983a4ac2d50cc1969cb0eaf644fcfde749a9e54
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-11-25 23:32:29 +0100

    table is filled with data

--- FootballAPI/index-script.js
+++ FootballAPI/index-script.js
@@ -10,20 +10,27 @@ function insertTable(data) {
   console.log("data", data);
   const element = document.getElementById("tableline");
 
-  let output = "";
+  let output = '<div class="flex flex-wrap">';
 
   data.forEach((team, index) => {
-    output += ' <div class="flex items-center rounded-2xl bg-tablebg p-4 gap-4">';
-    output += "<div>" + (index + 1) + "</div>";
+    output +=
+      ' <div class="flex items-center rounded-2xl bg-tablebg p-4 gap-4 w-72 mt-7 ml-7 h-16">';
+    output += "<div class=font-bold>" + (index + 1) + "</div>";
     output += ' <div class="w-8"> <img src="' + team.teamIconUrl + '"/></div>';
-    output += " <div>" + team.teamName + "</div>";
-    output += " <div>" + team.points + "</div>";
+    output += " <div class='flex-grow'>" + team.teamName + "</div>";
+    output += " <div class='font-bold ml-auto'>" + team.points + "</div>";
     output += "</div>";
+
+    if ((index + 1) % 2 == 0) {
+      output += '</div><div class="flex flex-wrap">';
+    }
   });
-  
+
+  output += "</div>";
 
   console.log("output", output);
   element.innerHTML = output;
 }
+
 
 loadTable();
---
+++ FootballAPI/page3-script.js
@@ -0,0 +1,44 @@
+function loadTable() {
+    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) =>
+      res.json().then((data) => {
+        insertTable(data);
+      })
+    );
+  }
+  
+  function insertTable(data) {
+    console.log("data", data);
+    const element = document.getElementById("tableline");
+  
+    let output = '<div class="flex flex-wrap">';
+  
+    data.forEach((team, index) => {
+      output +=
+        ' <div class="flex items-center rounded-2xl bg-tablebg p-4 gap-4 w-80 mt-7 ml-7 h-16">';
+      output += "<div class=font-bold>" + (index + 1) + "</div>";
+      output += ' <div class="w-8"> <img src="' + team.teamIconUrl + '"/></div>';
+      output += " <div class='flex-grow'>" + team.teamName + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.matches + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.won + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.draw + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.lost + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.goals + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.opponentGoals + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.goalDiff + "</div>";
+      output += " <div class='font-bold ml-auto'>" + team.points + "</div>";
+      output += "</div>";
+  
+      if ((index + 1) % 2 == 0) {
+        output += '</div><div class="flex flex-wrap">';
+      }
+    });
+  
+    output += "</div>";
+  
+    console.log("output", output);
+    element.innerHTML = output;
+  }
+  
+  
+  loadTable();
+  
