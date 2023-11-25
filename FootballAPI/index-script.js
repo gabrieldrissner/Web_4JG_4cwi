4b5776a7dcb0d6252d2d9c5d3616a8067077a561
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-11-25 18:47:58 +0100

    work on the live data

--- FootballAPI/index-script.js
+++ FootballAPI/index-script.js
@@ -1,33 +1,29 @@
 function loadTable() {
-  fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().then((data) => {
+  fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) =>
+    res.json().then((data) => {
       insertTable(data);
-  }))
-
+    })
+  );
 }
 
 function insertTable(data) {
   console.log("data", data);
-  const element = document.getElementById("tabelle");
+  const element = document.getElementById("tableline");
 
-  let output = '';
+  let output = "";
+
   data.forEach((team, index) => {
-      output += ' <div class="grid grid-cols-[20px_40px_auto_40px] p-2 rounded-xl border border-black border-solid mb-2 hover:bg-slate-400">';
-      output += "<div>" + (index + 1) + "</div>";
-      output += ' <div class="w-5"> <img src="' + team.teamIconUrl + '"/></div>';
-      output += " <div>" + team.teamName + "</div>";
-      output += ' <div>' + team.points + '</div>';
-      output += '</div>';
+    output += ' <div class="flex items-center rounded-2xl bg-tablebg p-4 gap-4">';
+    output += "<div>" + (index + 1) + "</div>";
+    output += ' <div class="w-8"> <img src="' + team.teamIconUrl + '"/></div>';
+    output += " <div>" + team.teamName + "</div>";
+    output += " <div>" + team.points + "</div>";
+    output += "</div>";
   });
+  
 
   console.log("output", output);
   element.innerHTML = output;
-
-
 }
 
-
 loadTable();
-
-
-
-
